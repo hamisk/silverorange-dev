4dade39df6ce8db02412824202106da263998b29
hamisk <hs.khera@icloud.com>
2022-05-12 10:44:08 -0700

    Store repo data in state

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
 export function App() {
+  const [repoList, setRepoList] = useState<any>([]);
+
   useEffect(() => {
     axios
       .get('http://localhost:4000/repos')
       .then((res) => {
-        console.log(res.data);
+        setRepoList(res.data);
       })
       .catch((err) => console.log(err));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <div className="App"> React App</div>;
 }
