f2e77ece02b73b2ab86c8fdebadee35d0d0a0a15
hamisk <hs.khera@icloud.com>
2022-05-12 12:32:56 -0700

    Add functionality to filter by language on button click

--- web/src/pages/RepoListPage/RepoListPage.tsx
+++ web/src/pages/RepoListPage/RepoListPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import RepoListItem from '../../components/RepoListItem/RepoListItem';
 import { Repo } from '../../models/Repo';
 
@@ -17,6 +17,17 @@ export function RepoListPage({
     new Set(repoList.map((repo) => repo.language))
   );
 
+  useLayoutEffect(() => {
+    setReposToDisplay(repoList);
+  }, [repoList]);
+
+  const [reposToDisplay, setReposToDisplay] = useState(repoList);
+
+  const filterByLanguage = (language: string) => {
+    const filteredRepos = repoList.filter((repo) => repo.language === language);
+    setReposToDisplay(filteredRepos);
+  };
+
   return (
     <div className="repo-list">
       <h2 className="repo-list__title">A list of repositories</h2>
@@ -29,11 +40,15 @@ export function RepoListPage({
         ''
       )}
       {listOfLanguages.map((language, index) => (
-        <button className="repo-list__language" key={language + index}>
+        <button
+          className="repo-list__language"
+          key={language + index}
+          onClick={() => filterByLanguage(language)}
+        >
           {language}
         </button>
       ))}
-      {repoList.map((repo: any) => (
+      {reposToDisplay.map((repo: Repo) => (
         <RepoListItem
           key={repo.id}
           name={repo.name}
