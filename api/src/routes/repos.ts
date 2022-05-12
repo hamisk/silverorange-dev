f459fae9c1d5a7bd730d06b33eacb73a5e50869c
hamisk <hs.khera@icloud.com>
2022-05-12 10:08:14 -0700

    Retrieve repo data from github source

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from 'express';
 import fs from 'fs';
+import axios from 'axios';
+import { Repo } from '../models/Repo';
 
 export const repos = Router();
 
@@ -10,9 +12,19 @@ repos.get('/', async (_: Request, res: Response) => {
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
   // Read local repos.json data when endpoint called to account for any changes to file while server running
-  const localReposData = JSON.parse(
+  const localReposData: Repo = JSON.parse(
     fs.readFileSync('./data/repos.json', 'utf8')
   );
+
+  axios
+    .get('https://api.github.com/users/silverorange/repos')
+    .then((resp) => {
+      const githubReposData: Repo[] = resp.data;
+    })
+    .catch((err) => {
+      // Catch error if unable to access external github endpoint
+      res.status(500).send(`Error retrieving repos from github: ${err}`);
+    });
 
   res.json([localReposData]);
 });
