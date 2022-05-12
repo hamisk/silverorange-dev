f8f55120993bee9abafd48380a3b0f24be3ee028
hamisk <hs.khera@icloud.com>
2022-05-12 10:58:21 -0700

    Add button to retry when retrieving repo data fails

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,16 +4,41 @@ import './App.css';
 
 export function App() {
   const [repoList, setRepoList] = useState<any>([]);
+  const [returnedError, setReturnedError] = useState<boolean>(false);
 
   useEffect(() => {
+    getRepoData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getRepoData = () => {
+    setReturnedError(false);
+
     axios
       .get('http://localhost:4000/repos')
       .then((res) => {
         setRepoList(res.data);
+        console.log(repoList);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        setReturnedError(true);
+      });
+  };
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return <div className="App"> React App</div>;
+  return (
+    <div className="App">
+      <div className="repo-list">
+        <h2 className="repo-list__title">A list of repositories</h2>
+        {returnedError ? (
+          <>
+            <p className="repo-list__text">Error retrieving repository data</p>
+            <button onClick={() => getRepoData()}>Click to retry</button>
+          </>
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
+  );
 }
